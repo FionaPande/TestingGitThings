034eaa13997854baf2bd4de779ac47e1a5589294
Fiona privat <neumann.fiona@outlook.com>
2023-01-06 12:37:28 +0200

    delete

--- feature-wrong.js
+++
@@ -1,6 +0,0 @@
-// we worked on our feature in another branch here
-// but were supposed to do it from dev
-
-// made a first commit to wrong base branch
-
-// made a second commit that should be squashed
